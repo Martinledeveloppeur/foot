97e4cb3bfd1008d90f0f5b76a6b4f9d0b616adcb
Nestouu <33mathiaspetit@gmail.com>
2023-03-03 12:54:44 +0100

    routes2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :cards
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  Defines the root path route ("/")
+  #Defines the root path route ("/")
   root "articles#index"
 end
